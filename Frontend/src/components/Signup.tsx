f55441aa94e56e9983c1247ce549a63857ec7b4d
Shaik Yaseen <hackercanhack69@gmail.com>
2024-11-30 01:37:02 +0530

    clinic bugs

--- Frontend/src/components/Signup.tsx
+++ Frontend/src/components/Signup.tsx
@@ -4,6 +4,7 @@ import { Button } from './ui/Button'
 import { Link, useNavigate } from 'react-router-dom'
 import { Signupauth } from '../services/auth'
 import toast, { Toaster } from 'react-hot-toast'
+import Loader from './ui/Loader'
 
 const Signup = () => {
     const [username,setUsername] = useState("");
@@ -24,29 +25,35 @@ const Signup = () => {
        setLoading(false);
     }
 
-  return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center bg-black'>
-    <Toaster/>
-        <h1 className='text-white font-bold text-[30px]'>Signup</h1>
-        <div className='h-[400px] w-[300px] bg-gray-300 rounded-2xl pt-7 flex flex-col justify-center items-center'>
-        <div className='mb-2 p-2'>
-        <p className='font-bold pb-1'>Username :</p>
-        <Input size="md" type="text" onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your name."/>
-         </div>
-        <div className='mb-2 p-2'>
-        <p className='font-bold pb-1'>Email :</p>
-        <Input size="md" type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter your email."/>
-         </div>
-      <div className='p-2'>
-      <p className='font-bold pb-1'>Password :</p>
-        <Input size="md" type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Enter your password'/>
-      </div>
-      <div className='mt-4'>
-        <Button loading={loading} onClick={()=>handleSubmit()} text="Signup" size="sm" variant="primary"/>
-        </div>
-        <p className='mt-2 text-[13px] pt-3'>already have an account ? <Link to="/login" className=''>login</Link></p>
-        </div>
+  return (<>
+  {
+    loading ? <Loader/>
+:
+<div className='w-screen h-screen flex flex-col justify-center items-center bg-black'>
+<Toaster/>
+    <h1 className='text-white font-bold text-[30px]'>Signup</h1>
+    <div className='h-[400px] w-[300px] bg-gray-300 rounded-2xl pt-7 flex flex-col justify-center items-center'>
+    <div className='mb-2 p-2'>
+    <p className='font-bold pb-1'>Username :</p>
+    <Input size="md" type="text" onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your name."/>
+     </div>
+    <div className='mb-2 p-2'>
+    <p className='font-bold pb-1'>Email :</p>
+    <Input size="md" type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter your email."/>
+     </div>
+  <div className='p-2'>
+  <p className='font-bold pb-1'>Password :</p>
+    <Input size="md" type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Enter your password'/>
+  </div>
+  <div className='mt-4'>
+    <Button loading={loading} onClick={()=>handleSubmit()} text="Signup" size="sm" variant="primary"/>
     </div>
+    <p className='mt-2 text-[13px] pt-3'>already have an account ? <Link to="/login" className=''>login</Link></p>
+    </div>
+</div>
+  }
+  </>
+   
   )
 }
 
