9b0847b60c543ad27d85f397e02fb9fb602045c3
Shaik Yaseen <hackercanhack69@gmail.com>
2024-12-01 21:28:51 +0530

    Added files

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "https://brainbox-1.onrender.com/",
+  origin: "https://brainbox-1.onrender.com",
   credentials: true,
 };
 
